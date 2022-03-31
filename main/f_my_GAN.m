29d6d23b986d32ab0b4908ce8d85290e028c2bc6
DuanMuXiaoAo <101321912+DuanMuXiaoAo@users.noreply.github.com>
2022-03-31 17:58:44 +0800

    chore: 无用注释删除，美化代码

--- main/f_my_GAN.m
+++ main/f_my_GAN.m
@@ -1,35 +1,7 @@
 function [I] = f_my_GAN(I_input,numEpochs,scale,im_size)
 
-%% Data
-% filepath_lr = '/Users/rxthinking/Desktop/imgs/';
-% filepath_gt = '/Users/rxthinking/Desktop/imgs/';
-% img_name_lr = 'im_3.png';
-% img_name_gt = 'img_3_gt.png';
-
-% augimds = augmentedImageDatastore([32 32 3], imds);
-%scale = 0.5;
 %% Discriminator
-% scale = 0.5;
-% layersDiscriminator = [
-%     imageInputLayer([32 32 3],"Name","imageinput","Normalization","none")
-%     SpectralNormalization(convolution2dLayer([7,7],64,"NumChannels", 3, 'Stride',1,'Padding','same','Name','conv1') , "sn1" )
-%     convolution2dLayer([1 1],64,"Name","conv_2","Padding","same")
-%     batchNormalizationLayer("Name","batchnorm_1")
-%     leakyReluLayer(scale,"Name","relu_1")
-%     convolution2dLayer([1 1],64,"Name","conv_3","Padding","same")
-%     batchNormalizationLayer("Name","batchnorm_2")
-%     leakyReluLayer(scale,"Name","relu_2")
-%     convolution2dLayer([1 1],64,"Name","conv_4","Padding","same")
-%     batchNormalizationLayer("Name","batchnorm_3")
-%     leakyReluLayer(scale,"Name","relu_3")
-%     convolution2dLayer([1 1],64,"Name","conv_5","Padding","same")
-%     batchNormalizationLayer("Name","batchnorm_4")
-%     leakyReluLayer(scale,"Name","relu_4")
-%     convolution2dLayer([1 1],64,"Name","conv_6","Padding","same")
-%     batchNormalizationLayer("Name","batchnorm_5")
-%     leakyReluLayer(scale,"Name","relu_5")
-%     convolution2dLayer([1 1],1,"Name","conv_7","Padding","same")
-% ];
+
 im_input_size = im_size(:,1:2);
 layersDiscriminator = [
     imageInputLayer(ceil(im_input_size./scale),"Name","imageinput","Normalization","none")
@@ -54,6 +26,7 @@ layersDiscriminator = [
 
 lgraphDiscriminator = layerGraph(layersDiscriminator);
 dlnetDiscriminator = dlnetwork(lgraphDiscriminator);
+
 %% Generator
 stride = scale;
 
@@ -67,48 +40,13 @@ imageInputLayer(im_input_size,"Name","imageinput","Normalization","none")
 ];
 
 
-% layersGenerator_2 = [
-% imageInputLayer([64 64 3],"Name","imageinput","Normalization","none")
-%     convolution2dLayer([7 7],64,"Name","conv_1","Padding","same")
-%     convolution2dLayer([5 5],64,"Name","conv_2","Padding","same")
-%     convolution2dLayer([3 3],64,"Name","conv_3","Padding","same")
-%     convolution2dLayer([1 1],64,"Name","conv_4","Padding","same")
-%     convolution2dLayer([1 1],3,"Name","conv_5","Padding","same","Stride",[stride stride])
-% ];
-
-
-
-% layersGenerator_final = [
-% imageInputLayer([64 64 3],"Name","imageinput","Normalization","none")
-%     convolution2dLayer([7 7],64,"Name","conv_1","Padding","same")
-%     convolution2dLayer([5 5],64,"Name","conv_2","Padding","same")
-%     convolution2dLayer([3 3],64,"Name","conv_3","Padding","same")
-%     convolution2dLayer([1 1],64,"Name","conv_4","Padding","same")
-%     convolution2dLayer([1 1],3,"Name","conv_5","Padding","same")
-% ];
-
-
 lgraphGenerator = layerGraph(layersGenerator);
-
-
-% lgraphGenerator_2 = layerGraph(layersGenerator_2);
-% lgraphGenerator_final = layerGraph(layersGenerator_final);
-
-% tempLayers = resize2dLayer("Name","resize-output-size","GeometricTransformMode","half-pixel","Method","nearest","NearestRoundingMode","round","OutputSize",[32 32]);
-% lgraphGenerator = addLayers(lgraphGenerator,tempLayers);
-% clear tempLayers;
-% lgraphGenerator = connectLayers(lgraphGenerator,"imageinput","resize-output-size");
-% lgraphGenerator = connectLayers(lgraphGenerator,"resize-output-size","crop2d/ref");
-
-% dlnetGenerator_final = dlnetwork(lgraphGenerator_final);
 
 dlnetGenerator = dlnetwork(lgraphGenerator);
 
 
-% dlnetGenerator_2 = dlnetwork(lgraphGenerator_2);
 %% 指定训练选项
 
-% numEpochs = 1000;
 miniBatchSize = 1;
 
 learnRate = 0.0002;
@@ -133,7 +71,6 @@ numValidationImages = 1;
 
 
 f = figure;
-% f.Position(3) = 2*f.Position(3);
 
 scoreAxes = subplot(1,1,1);
 
@@ -154,16 +91,12 @@ for epoch = 1:numEpochs
    if mod(epoch,750) == 0
          learnRate = 0.1*learnRate;
     end
-    % Reset and shuffle datastore.
-%     shuffle(mbq);
     
     % Loop over mini-batches.
         iteration = iteration + 1;
         
         % Read mini-batch of data.
-%         a = next(mbq);
         dlX = im2double(I_input);
-%         dlX = imresize(dlX,[64 64]);
         dlX = dlX(:,:,channel);
         dlX = imcrop(dlX,[1,1,31,31]);
         dlX = dlarray(dlX,'SSCB'); 
@@ -200,40 +133,6 @@ for epoch = 1:numEpochs
             trailingAvgGenerator, trailingAvgSqGenerator, iteration, ...
             learnRate, gradientDecayFactor, squaredGradientDecayFactor);
         
-        % Every validationFrequency iterations, display batch of generated images using the
-        % held-out generator input
-
-%         if mod(iteration,validationFrequency) == 0 || iteration == 1
-% %             dlnetGenerator_2.Learnables = dlnetGenerator.Learnables;
-%             % Generate images using the held-out generator input.
-% 
-%             ZValidation = im2double(imread([filepath_gt,img_name_gt]));
-%             [m,n,k] = size(ZValidation);
-%             I = zeros(m/stride,n/stride,3);
-%             for i = 1:3
-% 
-%                 % ZValidation = imcrop(ZValidation,[1 1 64 64]);
-% 
-%                 dlZValidation = dlarray(ZValidation(:,:,i),'SSCB');
-% 
-%                 if (executionEnvironment == "auto" && canUseGPU) || executionEnvironment == "gpu"
-%                     dlZValidation = gpuArray(dlZValidation);
-%                 end
-%                 dlXGeneratedValidation = predict(dlnetGenerator,dlZValidation);
-%                 I(:,:,i) = extractdata(dlXGeneratedValidation);
-%             end
-
-
-            % Tile and rescale the images in the range [0 1].
-%             I = imtile(extractdata(dlXGeneratedValidation));
-%             I = rescale(I);
-            
-            % Display the images.
-%             subplot(1,2,1);
-%             image(imageAxes,I)
-%             xticklabels([]);
-%             yticklabels([]);
-%             title("Generated Images");
         
         
         % Update the scores plot
@@ -269,23 +168,7 @@ dlXGeneratedNew = predict(dlnetGenerator,dlZNew);
 
  I(:,:,i) = extractdata(dlXGeneratedNew);
 end
-%  I = imresize(I,[64,64]);
-%  I = imcrop(I,[16,16,31,31]);
-% I = imtile(extractdata(dlXGeneratedNew));
-% I = rescale(I);
-% dlXreal = im2double(imread([filepath_lr,img_name_lr]));
-% dlXreal = imresize(dlXreal,[64 64]);
-% dlXreal = imcrop(dlXreal,[16,16,31,31]);
-% down_real = im2double(imread([filepath_gt,img_name_gt]));
-% figure
-% subplot(1,3,2),imshow(dlXreal)
-% title("Real DownScaled Images")
-% subplot(1,3,1),imshow(down_real)
-% title("Real Images")
-% [m,n,c] = size(dlXreal);
-% I = imresize(I,[m,n]);
-% subplot(1,3,3),imshow(I)
-% title("Generated Images")
+
 %% 定义模型梯度、损失函数和分数
 function [gradientsGenerator, gradientsDiscriminator, stateGenerator, scoreGenerator, scoreDiscriminator] = ...
     modelGradients(dlnetGenerator, dlnetDiscriminator, dlX, dlZ, flipFactor)
