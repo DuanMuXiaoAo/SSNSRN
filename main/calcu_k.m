9cc0b51c2a0844838eb513740960cd5c31a8455e
笑傲 端木 <372991816@qq.com>
2022-04-12 09:20:21 +0800

    feat: 模糊核提取的实现

--- main/calcu_k.m
+++ main/calcu_k.m
@@ -1,6 +1,22 @@
-function [kernel] = calcu_k(dlGenerator)
-    delta = double(ones(1,1,1,1);
-    dlGenerator.
-    for i = 1:2:4
-        dlnetGenerator.Learnables(i,:).Value{1}
+%function [kernel] = calcu_k(dlGenerator)
+    load('dlnet.mat')
+    delta = double(ones(1,1,1,1));
+    value = extractdata(dlnetGenerator.Learnables(1,:).Value{1});
+    dl = dlarray(value, 'SSCB');
+    [a,b,c,d] = size(dlnetGenerator.Learnables(1,:).Value{1});
+    conv_size =[a,b];
+    layer = [imageInputLayer(size(delta),"Name","imageinput","Normalization","none")
+        convolution2dLayer(conv_size,d,"Name","conv_2","Padding",12,'Weights',extractdata(dlnetGenerator.Learnables(1,:).Value{1}))];
+    dlnet = dlnetwork(layer);
+    filter = dlnet.forward(dl);
+    for i = 3:2:9
+        value = extractdata(dlnetGenerator.Learnables(i,:).Value{1});
+        dl = dlarray(value,'SSCB');
+        [a1,b1,c1,d1] = size(dl);
+        [a,b,c,d] = size(filter);
+        conv_size = [a,b];
+        layer = [imageInputLayer([a1,b1,c1],"Name","in","Normalization","none")
+            convolution2dLayer(conv_size,d,"Name","conv_2","Padding","same")];
+        dlnet = dlnetwork(layer);
+        filter = dlnet.forward(dl);
     end
